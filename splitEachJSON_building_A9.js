9c657fc0b6f6de1790dbb5e9f18e9eb757899a86
webseon@gmail.com <dreamFActory3!>
2021-03-03 14:57:03 +0900

    기초공사

--- splitEachJSON_building_A9.js
+++ splitEachJSON_building_A9.js
@@ -4,7 +4,8 @@ var shpPath = `geoJSON_building/result_100.json`;
 
 function splitGeojson() {
   console.log("\n *START* \n");
-  console.log('참조체계연계키 A21 기반 개별 파일화')
+  console.log('공동주택만 A9 기반 개별 파일화')
+  console.log('「건축법」에서는 공동주택의 종류와 범위를 아파트, 연립주택, 다세대주택 및 기숙사')
 
   var fileName = shpPath;
   var contents = fs.readFileSync(fileName);
@@ -13,15 +14,15 @@ function splitGeojson() {
   let num = 0;
   for (var key in jsonContent.features) {
     let feature = jsonContent.features[key];
-    features[feature['properties']['A21']] = feature;
+    if(feature['properties']['A9']==='공동주택') features[feature['properties']['A21']] = feature;
   }
   // console.log('features',features)
   for (var key in features) {
     // console.log(key)
     var jsonStr = JSON.stringify({...features[key]['properties'], geometry: features[key]['geometry']});
     // split json파일 생성
-    console.log(num++, `c:/result_A21/${key}.json`);
-    fs.writeFileSync(`c:/result_A21/${key}.json`, jsonStr);
+    console.log(num++, `c:/result_A9/${key}.json`);
+    fs.writeFileSync(`c:/result_A9/${key}.json`, jsonStr);
   }
 
   console.log("\n *EXIT* \n");
