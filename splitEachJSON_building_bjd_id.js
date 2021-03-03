3035f0bf754f0d57e6b10038606fc132a97b1ba6
webseon@gmail.com <dreamFActory3!>
2021-03-03 15:18:59 +0900

    기초공사

--- splitEachJSON_building_bjd_id.js
+++ splitEachJSON_building_bjd_id.js
@@ -4,7 +4,7 @@ var shpPath = `geoJSON_building/result_100.json`;
 
 function splitGeojson() {
   console.log("\n *START* \n");
-  console.log('공동주택만 A2 기반 멀티 병합 개별 파일화');
+  console.log('공동주택만 bjd_id 기반 멀티 병합 개별 파일화');
   console.log('「건축법」에서는 공동주택의 종류와 범위를 아파트, 연립주택, 다세대주택 및 기숙사');
 
   var fileName = shpPath;
@@ -15,20 +15,22 @@ function splitGeojson() {
   for (var key in jsonContent.features) {
     let feature = jsonContent.features[key];
     if (feature['properties']['A9'] === '공동주택') {
-      if (!features[feature['properties']['A2']]) features[feature['properties']['A2']] = [];
-      features[feature['properties']['A2']].push({
+      let bjd_id = feature['properties']['A3'].substr(0,5)+'-'+feature['properties']['A19']
+
+      if (!features[bjd_id]) features[bjd_id] = [];
+      features[bjd_id].push({
         ...feature['properties'],
         geometry: feature['geometry']
       });
     }
   }
-  // console.log('features',features)
+
   for (var key in features) {
     // console.log(key)
     var jsonStr = JSON.stringify(features[key]);
     // split json파일 생성
-    console.log(num++, `c:/result_A2/${key}.json`);
-    fs.writeFileSync(`c:/result_A2/${key}.json`, jsonStr);
+    console.log(num++, `c:/result_bjd_id/${key}.json`);
+    fs.writeFileSync(`c:/result_bjd_id/${key}.json`, jsonStr);
   }
 
   console.log("\n *EXIT* \n");
