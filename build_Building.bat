abb6d8ca2d2fc9fce44af351b612cb43ff823b71
webseon@gmail.com <dreamFActory3!>
2021-03-05 12:39:45 +0900

    데이터 정밀도 바꿈

--- build_Building.bat
+++ build_Building.bat
@@ -1,5 +1,5 @@
 @echo start geoJSON build
 call "C:\Program Files\QGIS 3.12\OSGeo4W.bat" start cmd.exe /k exit
-ogr2ogr -f GeoJSON -lco COORDINATE_PRECISION=3 -t_srs crs:84 geoJSON_building/result.json geoJSON_building/source.shp
+ogr2ogr -f GeoJSON -lco COORDINATE_PRECISION=9 -t_srs crs:84 geoJSON_building/result.json geoJSON_building/source.shp
 @echo end geoJSON build
 
